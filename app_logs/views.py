b66e1aeea2870fc665375576ef57761708038503
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-11-30 16:28:10 -0300

    cria html

--- app_logs/views.py
+++ app_logs/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
+def index(request):
+    """Página principal do app_log"""
+    return render(request, 'app_logs/index.html')
+
+def topics(request):
+    """Mostra os assuntos"""
+    topic = Topic.objects.order_by('date_added')
+
+    context = {'topics': topics}
+
+    return render(request, 'app_logs/topics.html', context)
