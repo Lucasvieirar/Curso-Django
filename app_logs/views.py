ce981af429d8ae6b72b72b8d0134713698cf1271
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-01-22 15:39:01 -0300

    Restringindo páginas apenas para quem está logado.

--- app_logs/views.py
+++ app_logs/views.py
@@ -3,11 +3,12 @@ from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     """Página principal do app_log"""
     return render(request, 'app_logs/index.html')
-
+@login_required
 def topics(request):
     """Mostra os assuntos"""
     topic = Topic.objects.order_by('date_added')
@@ -15,14 +16,14 @@ def topics(request):
     context = {'topics': topics}
 
     return render(request, 'app_logs/topics.html', context)
-
+@login_required
 def topic(request,  topic_id):
     """Mostra um único assunto"""
     topic = Topic.objects.get(id = topic_id)
     entries = topic.entry_set.order.by("-date_added")
     context = {'topic': topic, 'entries':entries}
     return render(request, 'app_logs/topic.html', context)
-
+@login_required
 def new_topic(request):
     """Adiciona um novo assunto."""
     if request.method != 'POST':
@@ -37,7 +38,7 @@ def new_topic(request):
             return HttpResponseRedirect(reverse('topics'))
     context = {'form': form}
     return render(request, 'app_logs/.html', context)
-
+@login_required
 def new_entry(request, topic_id):
     """Acrescenta uma nova entrada par aum assunto e particular"""
     topic = Topic.objects.get(id=topic_id)
@@ -56,7 +57,7 @@ def new_entry(request, topic_id):
         
     context = {'topic':topic, 'form': form}
     return render(request, 'app_logs/new_entry.html', context)
-
+@login_required
 def edit_entry(request, entry_id):
     """Edita uma entrada existente"""     
     entry = Entry.objects.get(id=entry_id)
--- estudoDjango/settings.py
+++ estudoDjango/settings.py
@@ -118,3 +118,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# URL DE LOGIN PAGE
+
+LOGIN_URL = '/user/login'
